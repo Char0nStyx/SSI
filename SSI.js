0f98910df6c2bb37eb91f5eaee58098c1ba0dffc
Char0n <charonstyxriver@gmail.com>
2024-09-13 09:09:27 +0200

    Create SSI.js

---
+++ SSI.js
@@ -0,0 +1,48 @@
+/**
+ * SSI
+ * it is expected that this code is loaded dynamically in mods.
+ * See modBase repo.
+ * users are expected to implement the following functions:
+ * inject(inj)
+ * modMain()
+ */
+
+function runInjector() {
+  const VERSION = 2;
+  //SSI -> ShellShock injector
+  console.log(`using SSI version ${VERSION})`);
+  let shellJs;
+  const oldOpenFunc = XMLHttpRequest.prototype.open;
+  const oldResponse = Object.getOwnPropertyDescriptor(
+    XMLHttpRequest.prototype,
+    "response",
+  );
+  XMLHttpRequest.prototype.open = function (...args) {
+    if (args[1] && args[1].includes("js/shellshock.js")) {
+      shellJs = this;
+    }
+    return oldOpenFunc.apply(this, args);
+  };
+  Object.defineProperty(XMLHttpRequest.prototype, "response", {
+    get: function () {
+      if (this == shellJs) {
+        return injectInternal(oldResponse.get.call(this));
+      }
+      return oldResponse.get.call(this);
+    },
+  });
+
+  const injectInternal = function (js) {
+    const inj = function (o, n) {
+      //NOTE! Due to some anti-cheat, using replaceAll for more than
+      //200000 characters crashes the game.
+      //If you need more than 200k chars, save the original replaceAll
+      //prototype function at the start of the page and then run that.
+      js = js.replaceAll(o, n);
+    };
+    //user-implemented functions
+    if (typeof inject === "function") inject(inj);
+    if (typeof modMain === "function") modMain();
+    return js;
+  };
+}
